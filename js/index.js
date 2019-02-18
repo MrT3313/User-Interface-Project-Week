87b71049bc784b1fd6627ee228f1d2e2ed45d5a6
MrT3313 <rturgeon13@gmail.com>
2019-02-18 13:22:15 -0600

    initial JS setup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// JS goes here
+// Select Menu Button
+    const menuBUTTON = document.querySelector('.nav-contentRight')
+    
+// Select Whole Nav
+    const nav = document.querySelector('.container-NAV')
+
+// -- LOGISTICS -- //
+    let counter = 0
+    console.log(counter)
+
+// Add Event Listener -- CLOSED MENU
+    menuBUTTON.addEventListener('click', () => {
+        console.log('you clicked the menu button')
+        if (counter % 2 === 0) {
+            nav.style.height = '100vh'
+            nav.style.width = '100vw'
+            nav.style.opacity = '.5'
+            ++counter
+        } 
+    })
+
+// Add Event Listener - OPEN MENU
+    nav.addEventListener('click', () => {
+        console.log('you have clicked the NAV')
+        if (counter % 2 !== 0) {
+            nav.style.height = 'auto'
+            ++counter
+        }
+    })
